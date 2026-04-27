4603b4c5985a889caa24ff8d9f8d1297078b6d56
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2026-04-27 23:10:31 +0600

    Create customTheme.dart

---
+++ Final/customTheme.dart
@@ -0,0 +1,27 @@
+final ThemeData customTheme = ThemeData (
+	useMaterial3: true,
+	colorScheme: ColorScheme.fromSeed(
+		seedColor: Colors.green,
+		secondary: Colors.yellow,
+	),
+	scaffoldBackgroundColor: Colors.white,
+	textTheme: TextTheme(
+		headlineLarge: TextStyle(
+			fontSize: 32,
+			fontWeight: FontWeight.bold,
+		),
+		bodyLarge: TextStyle(
+			fontSize: 16,
+			color: Colors.black,
+		)
+	),
+	appBarTheme: AppBarTheme(
+		centerTitle: true,
+		backgroundColor: Colors.green,
+	),
+	elevatedButtonTheme: ElevatedButtonThemeData(
+		style: ElevatedButton.styleFrom(
+			backgroundColor: Colors.blueAccent,
+		)
+	)
+)
